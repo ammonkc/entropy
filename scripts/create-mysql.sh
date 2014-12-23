ccff7030e81efb645dc889ef47b717731401f2a0
Ammon Casey <ammon@caseyohana.com>
2014-12-23 12:03:40 -1000

    fix database args array

--- scripts/create-mysql.sh
+++ scripts/create-mysql.sh
@@ -5,7 +5,7 @@ SQL=$2;
 mysql -uentropy -psecret -e "DROP DATABASE IF EXISTS $DB";
 mysql -uentropy -psecret -e "CREATE DATABASE $DB";
 
-unless SQL.nil?
-    mysql -uentropy -psecret DB < SQL;
+unless $2.nil? || $2 == ""
+    mysql -uentropy -psecret $1 < $2;
 end
 
