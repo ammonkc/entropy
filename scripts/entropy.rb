02e4ad41890704b0cca91c3bb4c498f5052976cf
Ammon Casey <ammon@caseyohana.com>
2015-01-23 14:48:18 -1000

    update changes from homestead

--- scripts/entropy.rb
+++ scripts/entropy.rb
@@ -24,9 +24,16 @@ class Entropy
     config.vm.network "forwarded_port", guest: 3306, host: 33060
     config.vm.network "forwarded_port", guest: 5432, host: 54320
 
+    # Add Custom Ports From Configuration
+    if settings.has_key?("ports")
+      settings["ports"].each do |port|
+        config.vm.network "forwarded_port", guest: port["guest"], host: port["host"], protocol: port["protocol"] ||= "tcp"
+      end
+    end
+
     # Configure The Public Key For SSH Access
     config.vm.provision "shell" do |s|
-      s.inline = "echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+      s.inline = "echo $1 | grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys || echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
       s.args = [File.read(File.expand_path(settings["authorize"]))]
     end
 
--- src/ListCommand.php
+++ src/ListCommand.php
@@ -27,7 +27,7 @@ class ListCommand extends Command {
 	 */
 	public function execute(InputInterface $input, OutputInterface $output)
 	{
-		$process = new Process('vagrant box list | grep entropy', realpath(__DIR__.'/../'), $_ENV, null, null);
+		$process = new Process('vagrant box list | grep entropy', realpath(__DIR__.'/../'), array_merge($_SERVER, $_ENV), null, null);
 
 		$process->run(function($type, $line) use ($output)
 		{
